7527b115ca8bfc4cad864eb4ab60be62cd6dbce4
Gkmattos <gkmfox@gmail.com>
2017-03-21 22:27:39 -0300

    Parquimetro + contaMag

---
+++ Parquimetro/src/contaMagica/ContaMagica.java
@@ -0,0 +1,25 @@
+package contaMagica;
+
+enum Categorias {Silver, Gold, Status}
+
+public class ContaMagica {
+	private String nomeCliente;
+	private double saldo;
+	private Categorias status;
+	private double valor;
+	private ContaMagica nome;
+	
+	public ContaMagica(String nome) {
+		
+	}
+
+	//public String getNomeCliente(){…}
+
+	//public BigDecimal getSaldo(){…}
+
+	//public Categorias getStatus(){…}
+
+	//public void deposito(BigDecimal valor) {…}
+
+	//public void retirada(BigDecimal valor) {…}
+}
